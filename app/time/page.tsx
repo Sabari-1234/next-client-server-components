979e32edbceaca2732c9d711efed5b92afedf2c2
sabari@FS <sabarinathan.sadaiyan@finsurge.tech>
2024-05-08 13:24:43 +0530

    dynamic page with cookies(),header,search params

--- app/time/page.tsx
+++ app/time/page.tsx
@@ -1,6 +1,10 @@
+import { cookies } from "next/headers";
 import React from "react";
 
 const page = () => {
+  const cookieStore = cookies();
+  const theme = cookieStore.get("theme");
+  console.log(theme);
   return (
     <div>
       time:
