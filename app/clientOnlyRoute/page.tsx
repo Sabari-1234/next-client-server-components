ed74c4760cdaab984de2c3f26b127136dfccd8f6
sabari@FS <sabarinathan.sadaiyan@finsurge.tech>
2024-05-08 14:20:02 +0530

    error resolved

--- app/clientOnlyRoute/page.tsx
+++ app/clientOnlyRoute/page.tsx
@@ -1,13 +1,13 @@
 "use client";
-import { serverFn } from "@/utils/server-only-utils";
+//import { serverFn } from "@/utils/server-only-utils";
 import React from "react";
 
 const page = () => {
-  const result = serverFn();
+  //const result = serverFn();
   return (
     <div>
       <h1>client rendered</h1>
-      <h1>{result}</h1>
+      {/* <h1>{result}</h1> */}
     </div>
   );
 };
