40133d594fb439b79f0e3bb918f8e36f4f2d3e3b
sabari@FS <sabarinathan.sadaiyan@finsurge.tech>
2024-05-08 13:12:49 +0530

    change server side compo to client side como

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,10 @@
+"use client";
+
 import React from "react";
 
 const page = () => {
-  console.log("server side compo runs in node not in browser ");
+  //   console.log("server side compo runs in node not in browser ");
+  console.log("client side compo not runs in node and runs in browser ");
   return (
     <div>
       <h1>about page</h1>
